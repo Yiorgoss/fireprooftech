66559a9798388c357bd758ce381f480a5d3a467a
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2024-02-23 13:35:20 +0200

    possible unreplicatabl iphone bug fix

--- src/lib/components/circle-image-section.svelte
+++ src/lib/components/circle-image-section.svelte
@@ -15,9 +15,9 @@
 >
 	<div class="flex flex-col items-start gap-6 md:flex-row md:items-center">
 		<div
-			class="aspect-square h-[200px] w-[200px] rounded-full border-8 bg-black/20 md:h-[200px] md:w-[200px]"
+			class="aspect-square h-[200px] w-[200px] rounded-full border-8 md:h-[200px] md:w-[200px]"
 		>
-			<ImageWrapper imageClass="rounded-full aspect-square" meta={[image]} {alt} />
+			<ImageWrapper imageClass="object-cover w-full h-full rounded-full aspect-square" meta={[image]} {alt} />
 		</div>
 		<div class="hidden h-2 w-2 rounded-full bg-border md:static"></div>
 		<div class="relative h-10">
@@ -26,7 +26,7 @@
 				<h4 class="relative flex h-10 w-[500px] items-start font-playfair text-2xl md:items-center">
 					<!-- height must match -->
 					{title}
-					<div class="absolute -left-[250%] bottom-0 top-0 -z-10 w-[300vw] bg-[#9EC0F2]">
+					<div class="absolute -left-[250%] bottom-0 top-0 -z-10 w-[300vw] bg-transparent md:bg-[#9EC0F2]">
 						<!-- could be worse   -->
 					</div>
 				</h4>
